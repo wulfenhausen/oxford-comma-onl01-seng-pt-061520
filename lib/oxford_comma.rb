db49985e3e7e11fc51fa7b80a95d27416987d37d
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 00:23:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -14,3 +14,18 @@ end
   
   oxford_comma(array)
   
+  
+  array = ["apple"]
+def oxford_comma(array)
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
+  end
+end
+
+oxford_comma(array)
