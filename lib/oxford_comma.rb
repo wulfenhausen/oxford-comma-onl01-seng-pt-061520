b6f1e962ef6ef92c6dc9f0d943fca8eed8aa95b4
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 00:23:55 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
-array = ["fiddleheads","okra","kohlrabi"]
-def oxford_comma(array)
-  if array.length == 2
-    array[-2] << " and "
+#array = ["fiddleheads","okra","kohlrabi"]
+#def oxford_comma(array)
+#  if array.length == 2
+#    array[-2] << " and "
 #  array.join
 #elseif array.length == 1
 #  array.join
