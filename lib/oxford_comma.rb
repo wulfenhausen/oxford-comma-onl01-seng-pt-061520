bc0292c3c9298e3079ea0b5d6e8a9ac9640d6493
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 00:28:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,9 +3,9 @@ def oxford_comma(array)
   if array.length == 2
     array[-2] << " and "
   array.join
-elseif array.length == 1
+elsif array.length == 1
   array.join
-  elseif array.length > 2
+  elsif array.length > 2
    array[-1].prepend "and "
    array.join(", ")
   end
