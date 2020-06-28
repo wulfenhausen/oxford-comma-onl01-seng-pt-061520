d76cb26c5748af8e4a716d098ea28389722fb210
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 00:21:11 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,13 @@
 array = ["fiddleheads","okra","kohlrabi"]
 def oxford_comma(array)
   if array.length == 2
-    array[-2].push " and "
+    array[-2] << " and "
   array.join
-elseif array.length ==1
+elseif array.length == 1
   array.join
   elseif array.length > 2
-    array[-1].prepend(" and ")
-    array.join
+    array[-1].prepend("and ")
+    array.join(", ")
   end
 end
   
