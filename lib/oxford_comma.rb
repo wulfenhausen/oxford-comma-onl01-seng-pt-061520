9ccc2587673fdc7d97c6b24d2b2fba9036f073be
William Han Findiesen <william.findiesen@gmail.com>
2020-06-28 00:29:05 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,31 @@
+array = ["fiddleheads","okra","kohlrabi"]
 def oxford_comma(array)
-
+  if array.length == 2
+    array[-2] << " and "
+  array.join
+elsif array.length == 1
+  array.join
+  elsif array.length > 2
+   array[-1].prepend "and "
+   array.join(", ")
+  end
 end
+
+  
+oxford_comma(array)
+  
+  
+#  array = ["apple"]
+#def oxford_comma(array)
+#  if array.length == 2
+#  array[-2] << " and "
+#  array.join
+#  elsif array.length == 1
+#  	array.join
+#  elsif array.length > 2
+#    array[-1].prepend "and "
+#	array.join(", ")
+#  end
+#end
+
+#oxford_comma(array)
